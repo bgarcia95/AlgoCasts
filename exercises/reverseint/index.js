77a1f0c6ef7e8df305e059b5215c225e3c37c55d
bgarcia95 <bgarcia.95@outlook.com>
2022-06-04 23:42:14 -0600

    complete integer reversal challenge

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    const reversedStringNumber = n.toString().split('').reverse().join('');
+
+    // Math.sign: Un número representando el signo del argumento dado. Si el argumento es un número positivo, negativo, cero positivo, o cero negativo, la función retornará 1, -1, 0 or -0 respectivamente. De lo contrario, retorna NaN.
+    // In this line Math.sign tell us if the passed number will be positive or negative
+    return parseInt(reversedStringNumber) * Math.sign(n);
+}
 
 module.exports = reverseInt;
