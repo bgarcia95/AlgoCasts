4574d4cb406ebe975a8fc5d2b688e45280cfb68c
bgarcia95 <bgarcia.95@outlook.com>
2022-06-04 23:41:44 -0600

    complete string reversal challenge

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution #1
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Solution #2
+function reverse(str) {
+    let reversed = '';
+
+    for (let character of str) {
+        debugger;
+        reversed = character + reversed;
+    }
+
+    return reversed;
+}
+
+// Solution #3
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((revChar, currChar) => currChar + revChar, '');
+// }
+
+// reverse('asdf');
 
 module.exports = reverse;
