81c60e9288893f15b3bf023b9100128da2ddadf6
bgarcia95 <bgarcia.95@outlook.com>
2022-06-04 23:42:25 -0600

    complete max chars challenge

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,52 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// My own solution
+// function maxChar(str) {
+//     const chars = {};
+
+//     for (let char of str) {
+//         chars[char] = chars[char] + 1 || 1;
+//     }
+
+//     const charsToArr = Object.values(chars);
+
+//     const maxValueChar = Math.max(...charsToArr);
+
+//     for (const char in chars) {
+//         if (chars[char] === maxValueChar) {
+//             return char;
+//         }
+//     }
+
+// }
+
+// Stephen's solution
+function maxChar(str) {
+    const chars = {};
+    let maxValue = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        if (chars[char]) {
+            chars[char]++;
+        } else {
+            chars[char] = 1;
+        }
+    }
+
+    // Looping through object, char in this case it will be the 'key' of that object property 
+    for (let char in chars) {
+        if (chars[char] > maxValue) {
+            maxValue = chars[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+
+}
+
+
 
 module.exports = maxChar;
